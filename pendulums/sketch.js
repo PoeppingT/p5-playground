cd173da673ed617abc8e9670aedca6ce1469b438
PoeppingT <PoeppingT@users.noreply.github.com>
2025-06-11 14:05:16 -0400

    add placeholder for pendulums, update index, and add basic rotatingLines

---
+++ pendulums/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  curves.push(randomCurve())
+  text('pendulums and double pendulums animation and control', 10, 10);
+}
+
+function draw() {
+  
+}
